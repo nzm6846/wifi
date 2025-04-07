ba66c5630b4c2f69a60a1aea7b9abed3e2eaac95
nzm <nzm6846@gmail.com>
2025-04-08 03:17:25 +0900

    feat: api데이터 받기 및 파싱

---
+++ src/main/java/kr/or/ddit/vo/WifiVo.java
@@ -0,0 +1,20 @@
+package kr.or.ddit.vo;
+
+public class WifiVo {
+	private String xSwifiMgrNo; 		// 관리번호
+	private String xSwifiWrdofc; 		// 자치구
+	private String xSwifiMainNm;	 	// 와이파이명
+	private String xSwifiAdres1;	 	// 도로명주소
+	private String xSwifiAdres2;	 	// 상세주소
+	private String xSwifiInstlFloor; 	// 설치위치(층)
+	private String xSwifiInstlTy;	 	// 설치유형
+	private String xSwifiInstlMby; 		// 설치기관
+	private String xSwifiSvcSe; 		// 서비스구분
+	private String xSwifiCmcwr; 		// 망종류
+	private String xSwifiCnstcYear; 	// 설치년도
+	private String xSwifiInoutDoor; 	// 실내외구분
+	private String xSwifiRemars3; 		// wifi접속환경
+	private String lat; 				// Y좌표
+	private String lnt; 				// X좌표
+	private String workDttm; 			// 작업일자
+}
--- src/main/java/kr/or/ddit/wifi/api/ApiExplorer.java
+++ src/main/java/kr/or/ddit/wifi/api/ApiExplorer.java
@@ -6,39 +6,87 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class ApiExplorer {
 	public static void main(String[] args) throws IOException {
-		StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088"); /*URL*/
-		urlBuilder.append("/" +  URLEncoder.encode("49437759697970753132346768544f76","UTF-8") ); /*인증키 (sample사용시에는 호출시 제한됩니다.)*/
-		urlBuilder.append("/" +  URLEncoder.encode("json","UTF-8") ); /*요청파일타입 (xml,xmlf,xls,json) */
-		urlBuilder.append("/" + URLEncoder.encode("CardSubwayStatsNew","UTF-8")); /*서비스명 (대소문자 구분 필수입니다.)*/
-		urlBuilder.append("/" + URLEncoder.encode("1","UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
-		urlBuilder.append("/" + URLEncoder.encode("5","UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
-		// 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
+		int start=1;
+		int end=10;
+		int total=0;
 		
-		// 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
-		urlBuilder.append("/" + URLEncoder.encode("20220301","UTF-8")); /* 서비스별 추가 요청인자들*/
-		
-		URL url = new URL(urlBuilder.toString());
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-type", "application/json");
-		System.out.println("Response code: " + conn.getResponseCode()); /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
-		BufferedReader rd;
-
-		// 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
-		if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+		do {
+			
+			StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088"); /*URL*/
+			urlBuilder.append("/" +  URLEncoder.encode("74626f746879707532354c6343456b","UTF-8") ); /*인증키 (sample사용시에는 호출시 제한됩니다.)*/
+			urlBuilder.append("/" +  URLEncoder.encode("json","UTF-8") ); /*요청파일타입 (xml,xmlf,xls,json) */
+			urlBuilder.append("/" + URLEncoder.encode("TbPublicWifiInfo","UTF-8")); /*서비스명 (대소문자 구분 필수입니다.)*/
+			urlBuilder.append("/" + URLEncoder.encode(String.valueOf(start),"UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
+			urlBuilder.append("/" + URLEncoder.encode(String.valueOf(end),"UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
+			// 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
+			
+			// 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
+			//urlBuilder.append("/" + URLEncoder.encode("20220301","UTF-8")); /* 서비스별 추가 요청인자들*/
+			
+			URL url = new URL(urlBuilder.toString());
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-type", "application/json");
+			System.out.println("Response code: " + conn.getResponseCode()); /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
+			BufferedReader rd;
+			
+			// 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
+			if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
 				rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		} else {
+			} else {
 				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-		}
-		StringBuilder sb = new StringBuilder();
-		String line;
-		while ((line = rd.readLine()) != null) {
+			}
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = rd.readLine()) != null) {
 				sb.append(line);
-		}
-		rd.close();
-		conn.disconnect();
-		System.out.println(sb.toString());
+			}
+			rd.close();
+			conn.disconnect();
+			System.out.println(sb.toString());
+			
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jobj = (JsonObject) jsonParser.parse(sb.toString());
+			JsonObject jsrow = (JsonObject) jobj.get("TbPublicWifiInfo");
+			
+			if(total==0) {
+				
+				total = jsrow.get("list_total_count").getAsInt();
+			}
+			
+			System.out.println(total);
+			JsonArray  jsarr = (JsonArray) jsrow.get("row");
+			
+			for(int i=0; i<jsarr.size(); i++) {
+				JsonObject obj = (JsonObject) jsarr.get(i);
+				System.out.println(obj.get("X_SWIFI_MGR_NO"));
+				System.out.println(obj.get("X_SWIFI_WRDOFC"));
+				System.out.println(obj.get("X_SWIFI_MAIN_NM"));
+				System.out.println(obj.get("X_SWIFI_ADRES1"));
+				System.out.println(obj.get("X_SWIFI_ADRES2"));
+				System.out.println(obj.get("X_SWIFI_INSTL_FLOOR"));
+				System.out.println(obj.get("X_SWIFI_INSTL_TY"));
+				System.out.println(obj.get("X_SWIFI_INSTL_MBY"));
+				System.out.println(obj.get("X_SWIFI_SVC_SE"));
+				System.out.println(obj.get("X_SWIFI_CMCWR"));
+				System.out.println(obj.get("X_SWIFI_CNSTC_YEAR"));
+				System.out.println(obj.get("X_SWIFI_INOUT_DOOR"));
+				System.out.println(obj.get("X_SWIFI_REMARS3"));
+				System.out.println(obj.get("LAT"));
+				System.out.println(obj.get("LNT"));
+				System.out.println(obj.get("WORK_DTTM"));
+				
+			} 
+			
+			start +=10;
+			end+=10;
+		}while(start<=total);
 	}
 }
